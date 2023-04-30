3252ef1d9a828ad9680ece57d550a601e4f05b7c
Radmir Shagiev <veselday@gmail.com>
2023-04-30 15:52:17 +0500

    Palindrome task

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int count,r,sum=0,t;
+
+Console.Write("Please write five-digit number: ");
+count = Convert.ToInt32(Console.ReadLine());
+
+if(count>9999 && count<100000)
+{
+for(t=count;count!=0;count=count/10)
+{
+r=count % 10;
+sum=sum*10+r;
+}
+if(t==sum)
+Console.Write($"{t} is a palindrome");
+else
+Console.Write($"{t} is not a palindrome");
+} else
+Console.Write("Please write five-digit number");
